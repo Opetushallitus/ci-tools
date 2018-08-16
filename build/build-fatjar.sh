a468b3c6dbfbe0674d9b0d34150bcb9ae42558d2
Ville Seppänen <ville.seppanen@gofore.com>
2018-08-16 13:04:25 +0300

    Clean build directory after building to prevent mixing builds in multi-artifact repositories

--- build/build-fatjar.sh
+++ build/build-fatjar.sh
@@ -8,5 +8,9 @@ sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfi
 cp ci-tools/run/run-fatjar.sh ${DOCKER_BUILD_DIR}/run
 
 docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
+find ${DOCKER_BUILD_DIR}
 docker images
 docker push ${DOCKER_TARGET}
+
+# Clean build dir to prevent mixing builds in multi-artifact repositories
+./ci-tools/common/clean-docker-build-dir.sh
