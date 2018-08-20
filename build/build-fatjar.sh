e64521ee295d4c2f7038faa1d50c1c1914f59275
Ville Seppänen <ville.seppanen@gofore.com>
2018-08-20 14:18:17 +0300

    Print build dir contents before attempting build

--- build/build-fatjar.sh
+++ build/build-fatjar.sh
@@ -7,8 +7,8 @@ cp ci-tools/build/Dockerfile-fatjar ${DOCKER_BUILD_DIR}/Dockerfile
 sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
 cp ci-tools/run/run-fatjar.sh ${DOCKER_BUILD_DIR}/run
 
-docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
 find ${DOCKER_BUILD_DIR}
+docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
 docker images
 docker push ${DOCKER_TARGET}
 
