32d3b21f8fc61d44e1fbb22330a68ac6451e462c
Ville Seppänen <ville.seppanen@gofore.com>
2018-08-22 10:15:58 +0300

    Use ci prefix in image tags

--- build/build-fatjar.sh
+++ build/build-fatjar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 export ARTIFACT_NAME=$1
-export DOCKER_TARGET="${ECR_REPO}/${ARTIFACT_NAME}:${TRAVIS_BUILD_NUMBER}"
+export BUILD_ID="ci-${TRAVIS_BUILD_NUMBER}"
+export DOCKER_TARGET="${ECR_REPO}/${ARTIFACT_NAME}:${BUILD_ID}"
 
 cp ci-tools/build/Dockerfile-fatjar ${DOCKER_BUILD_DIR}/Dockerfile
 sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
@@ -13,7 +14,7 @@ docker images
 docker push ${DOCKER_TARGET}
 
 export BUILD_TIMESTAMP=`TZ='Europe/Helsinki' date +'%Y-%m-%d %H:%M:%S %Z'`
-aws dynamodb put-item --table-name builds --item "{\"Service\": {\"S\": \"${ARTIFACT_NAME}\"}, \"Build\": {\"S\": \"ci-${TRAVIS_BUILD_NUMBER}\"}, \"Branch\": {\"S\": \"${TRAVIS_BRANCH}\"}, \"Commit\": {\"S\": \"${TRAVIS_COMMIT}\"}, \"Time\": {\"S\": \"${BUILD_TIMESTAMP}\"}}" --condition-expression "attribute_not_exists(Id)" --region eu-west-1
+aws dynamodb put-item --table-name builds --item "{\"Service\": {\"S\": \"${ARTIFACT_NAME}\"}, \"Build\": {\"S\": \"${BUILD_ID}\"}, \"Branch\": {\"S\": \"${TRAVIS_BRANCH}\"}, \"Commit\": {\"S\": \"${TRAVIS_COMMIT}\"}, \"Time\": {\"S\": \"${BUILD_TIMESTAMP}\"}}" --condition-expression "attribute_not_exists(Id)" --region eu-west-1
 
 # Clean build dir to prevent mixing builds in multi-artifact repositories
 ./ci-tools/common/clean-docker-build-dir.sh
