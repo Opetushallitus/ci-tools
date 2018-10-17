4722d853bfe34bc5265b18e559bf31579694d46d
Ville Seppänen <ville.seppanen@gofore.com>
2018-10-17 14:18:55 +0300

    PP-299: For master branch builds, tag the image also with latest-master

--- build/upload-image.sh
+++ build/upload-image.sh
@@ -7,6 +7,13 @@ export DOCKER_TARGET="${ECR_REPO}/${ARTIFACT_NAME}:${BUILD_ID}"
 echo "Uploading Docker image ${DOCKER_TARGET} to repository"
 docker push ${DOCKER_TARGET}
 
+if [ ${TRAVIS_BRANCH} == "master" ]; then
+  export MASTER_BRANCH_TAG="${ECR_REPO}/${ARTIFACT_NAME}:latest-master"
+  echo "We are in ${TRAVIS_BRANCH} branch, adding extra tag"
+  docker tag ${DOCKER_TARGET} ${MASTER_BRANCH_TAG}
+  docker push ${MASTER_BRANCH_TAG}
+fi
+
 echo "Updating build metadata to DynamoDB"
 export BUILD_TIMESTAMP=`TZ='Europe/Helsinki' date +'%Y-%m-%d %H:%M:%S %Z'`
 aws dynamodb put-item --table-name builds --item "{\"Service\": {\"S\": \"${ARTIFACT_NAME}\"}, \"Build\": {\"S\": \"${BUILD_ID}\"}, \"Branch\": {\"S\": \"${TRAVIS_BRANCH}\"}, \"Commit\": {\"S\": \"${TRAVIS_COMMIT}\"}, \"Time\": {\"S\": \"${BUILD_TIMESTAMP}\"}}" --condition-expression "attribute_not_exists(Id)" --region eu-west-1
