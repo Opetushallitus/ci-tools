0f6d60cd9109556a0c7da7aef3804cd9f403896f
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-01 10:05:21 +0200

    PP-413: Add support for run scripts from baseimages

--- build/build-war.sh
+++ build/build-war.sh
@@ -3,11 +3,22 @@
 export ARTIFACT_NAME=$1
 export BUILD_ID="ci-${TRAVIS_BUILD_NUMBER}"
 export DOCKER_TARGET="${ECR_REPO}/${ARTIFACT_NAME}:${BUILD_ID}"
-
-cp ci-tools/build/Dockerfile-war ${DOCKER_BUILD_DIR}/Dockerfile
-sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
-cp ci-tools/run/run-war.sh ${DOCKER_BUILD_DIR}/run
+ARTIFACT_DEST_PATH="/opt/tomcat/webapps/"
 
 find ${DOCKER_BUILD_DIR}
-docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
+
+if [[ $BASE_IMAGE == *"jdk11"* ]]; then
+    echo "Using run script from base image"
+    cp ci-tools/build/Dockerfile ${DOCKER_BUILD_DIR}/Dockerfile
+    sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
+    docker build --build-arg name=${ARTIFACT_NAME} --build-arg artifact_destination=${ARTIFACT_DEST_PATH} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
+else
+    echo "Copying legacy run script into docker image"
+    LEGACY_PATH="ci-tools/build/PP-413_run_scripts_for_old_baseimages"
+    cp $LEGACY_PATH/Dockerfile-war-run-from-ci-tools ${DOCKER_BUILD_DIR}/Dockerfile
+    cp $LEGACY_PATH/run/run-war.sh ${DOCKER_BUILD_DIR}/run
+    sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
+    docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
+fi
+
 docker images
