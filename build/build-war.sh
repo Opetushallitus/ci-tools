3a81ca1158965f6634194b38322e8642afaaf0fe
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-01 14:03:58 +0200

    PP-413: Inject runscripts only for base-legacy, all other baseimages already have it by default

--- build/build-war.sh
+++ build/build-war.sh
@@ -7,13 +7,13 @@ ARTIFACT_DEST_PATH="/opt/tomcat/webapps/"
 
 find ${DOCKER_BUILD_DIR}
 
-if [[ $BASE_IMAGE == *"jdk11"* ]]; then
+if [[ $BASE_IMAGE != *"base-legacy"* ]]; then
     echo "Using run script from base image"
     cp ci-tools/build/Dockerfile ${DOCKER_BUILD_DIR}/Dockerfile
     sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
     docker build --build-arg name=${ARTIFACT_NAME} --build-arg artifact_destination=${ARTIFACT_DEST_PATH} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
 else
-    echo "Copying legacy run script into docker image"
+    echo "Warning: You are using the deprecated base-legacy image. Update your .travis.yml to use a new baseimage."
     LEGACY_PATH="ci-tools/build/PP-413_run_scripts_for_old_baseimages"
     cp $LEGACY_PATH/Dockerfile-war-run-from-ci-tools ${DOCKER_BUILD_DIR}/Dockerfile
     cp $LEGACY_PATH/run/run-war.sh ${DOCKER_BUILD_DIR}/run
