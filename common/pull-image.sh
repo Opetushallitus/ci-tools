dee9712804374c1d03857f48ca6b06d62f4e7791
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-07 12:23:17 +0200

    PP-418: Force deprecation of base-legacy

--- common/pull-image.sh
+++ common/pull-image.sh
@@ -2,4 +2,16 @@
 
 export IMAGE_NAME="${1:-$BASE_IMAGE}"
 
-docker pull $ECR_REPO/$IMAGE_NAME
+# PP-418 Deprecate base-legacy
+if [[ $IMAGE_NAME == *"base-legacy"* ]]; then
+    echo "Error: You are using the deprecated base-legacy image. Update your .travis.yml to use a new baseimage."
+    echo "Use any one of the following:"
+    echo 'export BASE_IMAGE="baseimage-fatjar:master"'
+    echo 'export BASE_IMAGE="baseimage-fatjar:jdk11"'
+    echo 'export BASE_IMAGE="baseimage-war:master"'
+    echo 'export BASE_IMAGE="baseimage-war:jdk11"'
+    echo "Example: https://github.com/Opetushallitus/yki/blob/08a3577c20a6d172b4dbdb5bc8031ca739fd80b6/.travis.yml#L28"
+    travis_terminate 1 # This will break the build
+else
+    docker pull $ECR_REPO/$IMAGE_NAME
+fi
