32c067b178a6fefbc364d1c9555c2c8efd192acb
Ville Seppänen <ville.seppanen@gofore.com>
2019-03-12 09:33:05 +0200

    PP-418: Remove obsolete run scripts for base-legacy image. Run scripts are now in baseimage repos

--- build/build-war.sh
+++ build/build-war.sh
@@ -14,11 +14,7 @@ if [[ $BASE_IMAGE != *"base-legacy"* ]]; then
     docker build --build-arg name=${ARTIFACT_NAME} --build-arg artifact_destination=${ARTIFACT_DEST_PATH} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
 else
     echo "Warning: You are using the deprecated base-legacy image. Update your .travis.yml to use a new baseimage."
-    LEGACY_PATH="ci-tools/build/PP-413_run_scripts_for_old_baseimages"
-    cp $LEGACY_PATH/Dockerfile-war-run-from-ci-tools ${DOCKER_BUILD_DIR}/Dockerfile
-    cp $LEGACY_PATH/run/run-war.sh ${DOCKER_BUILD_DIR}/run
-    sed -i -e "s|BASEIMAGE|${ECR_REPO}/${BASE_IMAGE}|g" ${DOCKER_BUILD_DIR}/Dockerfile
-    docker build --build-arg name=${ARTIFACT_NAME} -t ${DOCKER_TARGET} ${DOCKER_BUILD_DIR}
+    travis_terminate 1
 fi
 
 docker images
