468443e51b9e1012c89e9525f2fb6686145234bd
Markku Sopula <markku.sopula@gofore.com>
2020-11-18 14:01:50 +0200

    Better/new Flowdock messages
    Add information that nginx package update is required
    Add list of packages that will be updated

--- version_check/package_versions.sh
+++ version_check/package_versions.sh
@@ -1,9 +1,27 @@
-#!/usr/bin/env sh
+#!/bin/bash
+set -eou pipefail
+
 . /etc/os-release
 case "$ID" in
 alpine) PACKAGES=$(apk info -v | sort) ;;
-ubuntu) PACKAGES=$(apt list --installed | awk '{ print $1, $2 }' | sort) ;;
+ubuntu) PACKAGES=$(apt list --installed | awk '{ print $1, $2 }' | sort)
+        NGINX=$(apt list --upgradeable nginx-extras || true)
+	NGINX=$(echo "${NGINX}"| grep -v Listing... || true)
+;;
 esac
+
+UPDATED=$(diff /repository/package-versions <(echo "$PACKAGES") || true)
+
+if [[ -n "${UPDATED}" ]]; then
+  echo "${UPDATED}" | grep ">"  | awk -F'[ /]' '{ print $2, $4 }' >  /repository/updated-packages
+  chmod 755 /repository/updated-packages
+fi
 
 echo "$PACKAGES" >/repository/package-versions && chmod 755 /repository/package-versions
 echo "$ID" > /repository/distro && chmod 755 /repository/distro
+
+if [[ -n "${NGINX}" ]]; then
+   NGINX=$(echo "${NGINX}" | awk -F'[ /\]]' '{ print "ngingx-extras needs to be manually updated from version", $7, "to version", $3 }')
+   echo "${NGINX}" > /repository/nginx-updates && chmod 755 /repository/nginx-updates
+fi
+
