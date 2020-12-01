f341147e2e8e9705bed14852000c37d6df3f478f
Markku Sopula <markku.sopula@gofore.com>
2020-12-01 08:39:31 +0200

    fixed script not to fail when no nginx changes

--- version_check/package_versions.sh
+++ version_check/package_versions.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 case "$ID" in
 alpine) PACKAGES=$(apk info -v | sort) ;;
 ubuntu) PACKAGES=$(apt list --installed | awk '{ print $1, $2 }' | sort)
-        NGINX=$(apt list --upgradeable nginx-extras | grep -v Listing...)
+        NGINX=$(apt list --upgradeable nginx-extras || true | grep -v Listing...)
 ;;
 esac
 
@@ -16,9 +16,9 @@ if [[ -n "${UPDATED}" ]]; then
   chmod 755 /repository/updated-packages
 fi
 
-
 echo "$PACKAGES" >/repository/package-versions && chmod 755 /repository/package-versions
 echo "$ID" > /repository/distro && chmod 755 /repository/distro
+
 if [[ -n "${NGINX}" ]]; then
    NGINX=$(echo "${NGINX}" | awk -F'[ /\]]' '{ print "ngingx-extras needs to be manually udpated from version", $7, "to version", $3 }')
    echo "${NGINX}" > /repository/nginx-updates && chmod 755 /repository/nginx-updates
